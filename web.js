ddc2d540af1ded6da144957070fe2dc73aa095ba
Adam Waxman <awaxman11@gmail.com>
2014-07-09 08:02:36 -0400

    Update web.js file and package.json file take 3

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var http = require('http');
 var gzippo = require('gzippo');
-
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 var app = express();
-app.use(express.logger());
-app.use(gzippo.staticGzip('' + __dirname));
 
-var server = http.createServer(app);
-server.listen(process.env.PORT || 5000);
+app.use(morgan('dev'));           // log every request to the console
+app.use(bodyParser());  
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
