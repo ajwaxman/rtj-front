bba0071198debf83abdf61102e68518a69a56e30
Adam Waxman <awaxman11@gmail.com>
2014-07-09 07:38:27 -0400

    scaffold angular app using yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name rtjFrontApp
+ * @description
+ * # rtjFrontApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('rtjFrontApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rtjFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rtjFrontApp
+ */
+angular.module('rtjFrontApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
